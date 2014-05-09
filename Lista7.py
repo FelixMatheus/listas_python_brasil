dd3f735f780a54efcacf459f64300ca4c020acb7
Matheus Felix <matheusfelix@spem0244.(none)>
2014-05-09 12:53:13 -0300

    [UPDATE] Lista 8 e Lista 7

--- Lista7.py
+++ Lista7.py
@@ -1,72 +1,98 @@
+#coding: utf-8
+
 #"Estrutura Sequencial"
 #Fonte: http://www.python.org.br/wiki/ListaDeExercicios
 #Name: Matheus Felix
 
 #Exercicio Extra 1 (json)
 
-#===============================================================================
-# import json
-#  
-# nome=""
-# result=""
-# with open("contatos.json", "w") as contatos:
-#     contato = {}
-#     contato['nome'] = 'nome'
-#     contato['tel'] = 'tel'
-#     contatos.write(json.dumps(contato))
-#      
-# i = 0    
-# contato = {}
-#  
-# with open("contatos.json", "a") as contatos:
-#     while (i == 0):
-#         nome = raw_input("Digite o nome: ")
-#         tel = raw_input ("Digite o tel: ")
-#         result += "\n"+ nome + "\t" +  tel
-#         contato['nome'] = str(nome)
-#         contato['tel'] = str(tel)
-#         contatos.write(json.dumps(contato))
-#          
-#         if (nome=="sair"):
-#             i = 1
-#===============================================================================
+import json
+  
+nome=""
+result=""
+with open("contatos.json", "w") as contatos:
+    contato = {}
+    contato['nome'] = 'nome'
+    contato['tel'] = 'tel'
+    contatos.write(json.dumps(contato))
+      
+i = 0    
+contato = {}
+  
+with open("contatos.json", "a") as contatos:
+    while (i == 0):
+        nome = raw_input("Digite o nome: ")
+        tel = raw_input ("Digite o tel: ")
+        result += "\n"+ nome + "\t" +  tel
+        contato['nome'] = str(nome)
+        contato['tel'] = str(tel)
+        contatos.write(json.dumps(contato))
+          
+        if (nome=="sair"):
+            i = 1
 
-#===============================================================================
-# 
-# #Exercicio Extra 1 (txt)
-# 
-# nome=""
-# result=""
-# while (nome!="sair"):
-#     nome = raw_input("Digite o nome: ")
-#     if (nome=="sair"):
-#         break
-#     tel = raw_input ("Digite o tel: ")
-#     result += "\n"+ nome + "\t" +  tel
-#      
-# f=open("contatos.txt","w")
-# f.write("Nome\tTelefone" + result)
-# f.close()
-# 
-# #Exercicio Extra 2
-# 
-# f=open("contatos.txt","r")
-# 
-# lista = f.readlines()
-# for i in lista:
-#     print (i)
-# f.close()
-#===============================================================================
+ 
+#Exercicio Extra 1 (txt)
+ 
+nome=""
+result=""
+while (nome!="sair"):
+    nome = raw_input("Digite o nome: ")
+    if (nome=="sair"):
+        break
+    tel = raw_input ("Digite o tel: ")
+    result += "\n"+ nome + "\t" +  tel
+      
+f=open("contatos.txt","w")
+f.write("Nome\tTelefone" + result)
+f.close()
+ 
+#Exercicio Extra 2
+ 
+f=open("contatos.txt","r")
+ 
+lista = f.readlines()
+for i in lista:
+    print (i)
+f.close()
+
+#Exercicio Extra 3
+
+tipo=""
+result=""
+while (tipo!="sair"):
+    tipo = raw_input("Digite o tipo do navio: ")
+    if (tipo=="sair"):
+        break
+    latitude = raw_input("Digite a Latitude: ")
+    longitude = raw_input("Digite a Longitude: ")
+    hora = raw_input ("Digite a hora: ")
+    result += "\n"+ tipo + "\t" +  latitude + "\t\t" + longitude + "\t\t" + hora
+ 
+print ("Navio\tLatitude\tLongitude\tHora" + result)
+ 
+f=open("frota.txt","w")
+f.write("Navio\tLatitude\tLongitude\tHora" + result)
+f.close()
+
+#Exercicio Extra 4
+
+f=open("frota.txt", "r")
+ 
+lista = f.readlines()
+for i in lista:
+        print(i)
+f.close()
 
 #Exercicio 1 (Python Brasil)
 
 f=open("ip.txt","r")
 lista = f.readlines()
 f.close()
-
+ 
 ipvalido = ""
 ipnaovalido = ""
-
+ 
 for i in range (len(lista)):
     print len(lista), "TAMANHO DA LISTA"
     lista2 = lista[i].rsplit(".")
@@ -77,20 +103,44 @@ for i in range (len(lista)):
     else:
         ipnaovalido += lista[i] + "\n"
         print ipnaovalido, "IPNAOVALIDO"
-
+ 
 f=open("ipvalido.txt","w")
 f.write(ipvalido)
 f.close()
-
+ 
 f=open("ipnaovalido.txt","w")
 f.write(ipnaovalido)
 f.close()
+    
+#Exercicio 2 Python Brasil
 
-print ("arquivos gerados com sucesso!")
+def conversao(num):
+    return ((num/1024)/1024)
+
+def percentual(lista,num):    
+    return ((float(num)*100)/sum(lista))
+
+f=open("usuarios.txt","r")
+lista = f.readlines()
+f.close()
+
+result="ACME Inc.               Uso do espaço em disco pelos usuários\n"
+result+="----------------------------------------------------------------\n"
+result+="Nr.\tUsuário\t\tEspaço utilizado\t% do uso\n"    
     
-    
-    
-    
+listaUsuario=[]
+listaEspaco=[]
+listaUtilizacao=[]
+
+for i in lista:
+    listaUsuario.append(i[0:16])
+    listaEspaco.append(conversao(int(i[16:])))
+
+for i in range (len(listaUsuario)):
+    listaUtilizacao.append(percentual(listaEspaco, listaEspaco[i]))
+    result += str(i+1) + "\t" + listaUsuario[i]  +  str("%.2f" %(listaEspaco[i])) + "\tMB" + "\t\t" + str("%.2f" %(listaUtilizacao[i])) + "\n"
+
+print (result)    
     
     
 
--- Lista8.py
+++ Lista8.py
@@ -28,24 +28,18 @@ class Bola:
         def mostraCor(self):
             print self.cor
 
-# class Teste(object):
-    #===========================================================================
-    # cor = raw_input("Digite uma cor: ")
-    # Material = raw_input("Digite um material: ")
-    # bola = Bola(cor, Material)
-    # bola.mostraCor()
-    # cor = raw_input("Digite uma cor: ")
-    # bola.trocaCor(cor)
-    # bola.mostraCor()
-    # mat = bola.Material
-    # print mat
-    #===========================================================================
+class Teste(object):
+    cor = raw_input("Digite uma cor: ")
+    Material = raw_input("Digite um material: ")
+    bola = Bola(cor, Material)
+    bola.mostraCor()
+    cor = raw_input("Digite uma cor: ")
+    bola.trocaCor(cor)
+    bola.mostraCor()
+    mat = bola.Material
+    print mat
 
 # Exercicio 2
-# Classe Quadrado: Crie uma classe que modele um quadrado:
-# 
-# Atributos: Tamanho do lado
-# Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
 
 class Quadrado:
     
@@ -58,29 +52,16 @@ class Quadrado:
     def calcular_area(self):
         return (self.lado)**2
 
-#===============================================================================
-# class Teste(object):
-#     lado = int(input("Digite um valor de lado: "))
-#     quadrado = Quadrado(lado)
-#     lado = int(input("Digite um valor de lado: "))
-#     quadrado.mudarLado(lado)
-#     retorno=quadrado.retornarLado()
-#     area=quadrado.calcular_area()
-#     print retorno, area
-#===============================================================================
+class Teste1(object):
+    lado = int(input("Digite um valor de lado: "))
+    quadrado = Quadrado(lado)
+    lado = int(input("Digite um valor de lado: "))
+    quadrado.mudarLado(lado)
+    retorno=quadrado.retornarLado()
+    area=quadrado.calcular_area()
+    print retorno, area
 
 # Exercicio 3
-
-#===============================================================================
-# Classe Retangulo: Crie uma classe que modele um retangulo:
-# 
-# Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
-# Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
-# Crie um programa que utilize esta classe. 
-# Ele deve pedir ao usuário que informe as medidades de um local.
-# Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés 
-# necessárias para o local.
-#===============================================================================
 
 class Retangulo:
     
@@ -95,53 +76,43 @@ class Retangulo:
     def calcula_perimetro(self):
         return (2*(self.lado_a))+(2*(self.lado_b))
     
-#===============================================================================
-# class Teste(object):
-#     lado_a=int(input("Digite o lado a: "))
-#     lado_b=int(input("Digite o lado b: "))
-#     retangulo = Retangulo(lado_a,lado_b)
-#     lado_a=int(input("Digite o lado a: "))
-#     lado_b=int(input("Digite o lado b: "))
-#     retangulo.mudarLado(lado_a,lado_b)
-#     area = retangulo.calcula_area()
-#     perimetro = retangulo.calcula_perimetro()
-#     print area, perimetro
-#===============================================================================
-
-#===============================================================================
-# class Teste(object):
-#     lado_a=float(input("Medida do Lado A: "))
-#     lado_b=float(input("Medida do Lado B: "))
-#     M1=lado_a
-#     M2=lado_b
-#     Local = Retangulo(M1,M2)
-#     lado_a=float(input("Medida do Lado A Piso: "))
-#     lado_b=float(input("Medida do Lado B Piso: "))
-#     P1=lado_a
-#     P2=lado_b
-#     Piso = Retangulo(P1,P2)
-#     area1 = Local.calcula_area()
-#     area2 = Piso.calcula_area()
-#     Qtd_Piso = area1/area2
-#     print area1, area2, Qtd_Piso
-#     lado_a=float(input("Medida Altura Rodape: "))
-#     lado_b=float(input("Medida Comprimento Rodape: "))
-#     Alt_R=lado_a
-#     Comp_R=lado_b
-#     Rodape = Retangulo(Alt_R, Comp_R)
-#     NumeroRodapes=(2*(M1/Comp_R))
-#     NumeroRodapes2=(2*(M2/Comp_R))
-#     TotalRodapes=(NumeroRodapes+NumeroRodapes2)
-#     print TotalRodapes
-#===============================================================================
+class Teste2(object):
+    lado_a=int(input("Digite o lado a: "))
+    lado_b=int(input("Digite o lado b: "))
+    retangulo = Retangulo(lado_a,lado_b)
+    lado_a=int(input("Digite o lado a: "))
+    lado_b=int(input("Digite o lado b: "))
+    retangulo.mudarLado(lado_a,lado_b)
+    area = retangulo.calcula_area()
+    perimetro = retangulo.calcula_perimetro()
+    print area, perimetro
+ 
+class Teste3(object):
+    lado_a=float(input("Medida do Lado A: "))
+    lado_b=float(input("Medida do Lado B: "))
+    M1=lado_a
+    M2=lado_b
+    Local = Retangulo(M1,M2)
+    lado_a=float(input("Medida do Lado A Piso: "))
+    lado_b=float(input("Medida do Lado B Piso: "))
+    P1=lado_a
+    P2=lado_b
+    Piso = Retangulo(P1,P2)
+    area1 = Local.calcula_area()
+    area2 = Piso.calcula_area()
+    Qtd_Piso = area1/area2
+    print area1, area2, Qtd_Piso
+    lado_a=float(input("Medida Altura Rodape: "))
+    lado_b=float(input("Medida Comprimento Rodape: "))
+    Alt_R=lado_a
+    Comp_R=lado_b
+    Rodape = Retangulo(Alt_R, Comp_R)
+    NumeroRodapes=(2*(M1/Comp_R))
+    NumeroRodapes2=(2*(M2/Comp_R))
+    TotalRodapes=(NumeroRodapes+NumeroRodapes2)
+    print TotalRodapes
 
 #Exercicio 4
-# #Classe Pessoa: Crie uma classe que modele uma pessoa:
-# # 
-# #Atributos: nome, idade, peso e altura
-# #Métodos: Envelhercer, engordar, emagrecer, crescer. 
-# Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, 
-# ela deve crescer 0,5 cm.
 
 class Pessoa:
     
@@ -166,22 +137,16 @@ class Pessoa:
     def envelhecer(self):
         return self.idade
     
-#===============================================================================
-# class Teste(object):
-#     
-#     nome = raw_input("Digite o Nome: ")
-#     idade = float(input("Digite a Idade: "))
-#     altura = float(input("Digite a Altura: "))
-#     peso = int(input("Digite o Peso: "))
-#     cresce = Pessoa(nome, idade, altura, peso)
-#     cresceu = cresce.crescer()
-#===============================================================================
+class Teste4(object):
+     
+    nome = raw_input("Digite o Nome: ")
+    idade = float(input("Digite a Idade: "))
+    altura = float(input("Digite a Altura: "))
+    peso = int(input("Digite o Peso: "))
+    cresce = Pessoa(nome, idade, altura, peso)
+    cresceu = cresce.crescer()
         
 #Exercicio 5
-# Classe Conta Corrente: Crie uma classe para implementar uma conta corrente.
-# A classe deve possuir os seguintes atributos: número da conta, nome do correntista e saldo. 
-# Os métodos são os seguintes: alterarNome, depósito e saque;
-# No construtor, saldo é opcional, com valor default zero e os demais atributos são obrigatórios.
 
 class Conta_Corrente:
     
@@ -200,9 +165,6 @@ class Conta_Corrente:
         return self.saldo
     
 #Exercicio 6
-# Classe TV: Faça um programa que simule um televisor criando-o como um objeto.
-# O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume. 
-# Certifique-se de que o número do canal e o nível do volume permanecem dentro de faixas válidas.
 
 class TV:
     
@@ -232,7 +194,7 @@ class TV:
         else:
             return "nada"
     
-class Teste(object):    
+class Teste5(object):    
      
     canal = int(input("Digite o Numero do Canal: "))
     volume = input("Digite o Volume: ")
@@ -243,62 +205,3 @@ class Teste(object):
     diminuir = TV(canal, volume, aum, dim)
     dim_vol = diminuir.diminuir_volume()
     print canal, volume, aum_vol, dim_vol
-    
-
-#Exercicio 7 
-# Classe Bichinho Virtual:Crie uma classe que modele um Tamagushi (Bichinho Eletrônico):
-# 
-# Atributos: Nome, Fome, Saúde e Idade b. Métodos: Alterar Nome, Fome, Saúde e Idade; Retornar Nome, Fome, Saúde e Idade Obs: Existe mais uma informação que devemos levar em consideração, o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fome e Saúde, ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação por que ela pode ser calculada a qualquer momento.
-
-#Exercicio 8
-# Classe Macaco: Desenvolva uma classe Macaco,que possua os atributos nome e bucho (estomago) e pelo menos os métodos comer(), verBucho() e digerir(). Faça um programa ou teste interativamente, criando pelo menos dois macacos, alimentando-os com pelo menos 3 alimentos diferentes e verificando o conteúdo do estomago a cada refeição. Experimente fazer com que um macaco coma o outro. É possível criar um macaco canibal?
-# 
-# Classe Ponto e Retangulo: Faça um programa completo utilizando funções e classes que:
-# 
-# Possua uma classe chamada Ponto, com os atributos x e y.
-# Possua uma classe chamada Retangulo, com os atributos largura e altura.
-# Possua uma função para imprimir os valores da classe Ponto
-# Possua uma função para encontrar o centro de um Retângulo.
-# Você deve criar alguns objetos da classe Retangulo.
-# Cada objeto deve ter um vértice de partida, por exemplo, o vértice inferior esquerdo do retângulo, que deve ser um objeto da classe Ponto.
-# A função para encontrar o centro do retângulo deve retornar o valor para um objeto do tipo ponto que indique os valores de x e y para o centro do objeto.
-# O valor do centro do objeto deve ser mostrado na tela
-# Crie um menu para alterar os valores do retângulo e imprimir o centro deste retângulo.
-# Classe Bomba de Combustível: Faça um programa completo utilizando classes e métodos que:
-# 
-# Possua uma classe chamada bombaCombustível, com no mínimo esses atributos:
-# tipoCombustivel.
-# valorLitro
-# quantidadeCombustivel
-# Possua no mínimo esses métodos:
-# abastecerPorValor( ) – método onde é informado o valor a ser abastecido e mostra a quantidade de litros que foi colocada no veículo
-# abastecerPorLitro( ) – método onde é informado a quantidade em litros de combustível e mostra o valor a ser pago pelo cliente.
-# alterarValor( ) – altera o valor do litro do combustível.
-# alterarCombustivel( ) – altera o tipo do combustível.
-# alterarQuantidadeCombustivel( ) – altera a quantidade de combustível restante na bomba.
-# OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba.
-# Classe carro: Implemente uma classe chamada Carro com as seguintes propriedades:
-# 
-# Um veículo tem um certo consumo de combustível (medidos em km / litro) e uma certa quantidade de combustível no tanque.
-# O consumo é especificado no construtor e o nível de combustível inicial é 0.
-# Forneça um método andar( ) que simule o ato de dirigir o veículo por uma certa distância, reduzindo o nível de combustível no tanque de gasolina.
-# Forneça um método obterGasolina( ), que retorna o nível atual de combustível.
-# Forneça um método adicionarGasolina( ), para abastecer o tanque. Exemplo de uso:
-# meuFusca = Carro(15);           # 15 quilômetros por litro de combustível. 
-# meuFusca.adicionarGasolina(20); # abastece com 20 litros de combustível. 
-# meuFusca.andar(100);            # anda 100 quilômetros.
-# meuFusca.obterGasolina()        # Imprime o combustível que resta no tanque.
-# Classe Conta de Investimento: Faça uma classe contaInvestimento que seja semelhante a classe contaBancaria, com a diferença de que se adicione um atributo taxaJuros. Forneça um construtor que configure tanto o saldo inicial como a taxa de juros. Forneça um método adicioneJuros (sem parâmetro explícito) que adicione juros à conta. Escreva um programa que construa uma poupança com um saldo inicial de R$1000,00 e uma taxa de juros de 10%. Depois aplique o método adicioneJuros() cinco vezes e imprime o saldo resultante.
-# 
-# Classe Funcionário: Implemente a classe Funcionário. Um empregado tem um nome (um string) e um salário(um double). Escreva um construtor com dois parâmetros (nome e salário) e métodos para devolver nome e salário. Escreva um pequeno programa que teste sua classe.
-# 
-# Aprimore a classe do exercício anterior para adicionar o método aumentarSalario (porcentualDeAumento) que aumente o salário do funcionário em uma certa porcentagem.
-# Exemplo de uso:
-#   harry=funcionário("Harry",25000)
-#   harry.aumentarSalario(10)
-# Classe Bichinho Virtual++: Melhore o programa do bichinho virtual, permitindo que o usuário especifique quanto de comida ele fornece ao bichinho e por quanto tempo ele brinca com o bichinho. Faça com que estes valores afetem quão rapidamente os níveis de fome e tédio caem.
-# 
-# Crie uma "porta escondida" no programa do programa do bichinho virtual que mostre os valores exatos dos atributos do objeto. Consiga isto mostrando o objeto quando uma opção secreta, não listada no menu, for informada na escolha do usuário. Dica: acrescente um método especial str() à classe Bichinho.
-# 
-# Crie uma Fazenda de Bichinhos instanciando vários objetos bichinho e mantendo o controle deles através de uma lista. Imite o funcionamento do programa básico, mas ao invés de exigis que o usuário tome conta de um único bichinho, exija que ele tome conta da fazenda inteira. Cada opção do menu deveria permitir que o usuário executasse uma ação para todos os bichinhos (alimentar todos os bichinhos, brincar com todos os bichinhos, ou ouvir a todos os bichinhos). Para tornar o programa mais interessante, dê para cada bichinho um nivel inicial aleatório de fome e tédio.
-#===============================================================================
